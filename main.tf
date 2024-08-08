201154da7b8e22f113b9f3cc28628154bb9e6873
Aksin Modak <153791824+aksin-modak@users.noreply.github.com>
2024-08-08 15:28:45 +0530

    Update main.tf
    Region update

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "tf16112023"
     key            = "terraform.tfstate"
-    region         = "us-east-1"
+    region         = "ap-south-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
   }
